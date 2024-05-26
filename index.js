d0b4c241c5e5446197ed784f839dd1031f265d43
Kevin Stout <stoutkev3@gmail.com>
2024-05-26 15:17:43 -0400

    messing with a wave shaper

--- index.js
+++ index.js
@@ -7,7 +7,38 @@ osc1.frequency.value = 440;
 const masterGain = audioContext.createGain();
 masterGain.gain.value = 0;
 
-osc1.connect(masterGain);
+const waveShaper = audioContext.createWaveShaper();
+
+function makeDistortionCurve(amount) {
+  var k = typeof amount === "number" ? amount : 50,
+    n_samples = 44100,
+    curve = new Float32Array(n_samples),
+    deg = Math.PI / 180,
+    i = 0,
+    x;
+  for (; i < n_samples; ++i) {
+    x = (i * 2) / n_samples - 1;
+    curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
+  }
+  return curve;
+}
+
+// function makeDistortionCurve(amount = 50) {
+//   let n_samples = 44100;
+//   let curve = new Float32Array(n_samples);
+//   let deg = Math.PI / 180;
+//   for (let i = 0; i < n_samples; ++i) {
+//     let x = (i * 2) / n_samples - 1;
+//     curve[i] = ((Math.PI / 2) * x) / (Math.PI / 2 - Math.abs(x) * amount);
+//   }
+//   return curve;
+// }
+
+waveShaper.curve = makeDistortionCurve(0);
+waveShaper.oversample = "4x";
+
+osc1.connect(waveShaper);
+waveShaper.connect(masterGain);
 masterGain.connect(audioContext.destination);
 
 osc1.start();
@@ -31,4 +62,8 @@ document.querySelector("#osc1Waveform").addEventListener("change", (e) => {
 
 document.querySelector("#osc1").addEventListener("input", (e) => {
   osc1.frequency.value = e.target.value;
+});
+
+document.querySelector("#waveShaper").addEventListener("input", (e) => {
+  waveShaper.curve = makeDistortionCurve(e.target.value);
 });
